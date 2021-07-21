1c940c856e3d6e3dbb4e1dcf8969f7df0e97913a
Christopher Homberger <christopher.homberger@web.de>
2021-07-21 22:36:36 +0200

    Fix commit

--- runnersurveyunsupported.go
+++ runnersurveyunsupported.go
@@ -1,5 +1,7 @@
 // +build !linux,!darwin,!windows,!openbsd,!netbsd,!freebsd
 
+package main
+
 func RunnerGroupSurvey(taskAgentPool string, taskAgentPools []string) string {
 	fmt.Printf("Survey disabled, due to incompatibility with some platforms:\nAvailable runner groups are [%v] using %v", taskAgentPools, taskAgentPool)
 	return taskAgentPool
