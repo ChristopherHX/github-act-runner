79ee7fa83c6f28039f62aba3c069624c46e2d5cd
Christopher Homberger <christopher.homberger@web.de>
2022-07-04 17:25:28 +0200

    Use a waitgroup instead of replacing an context
    Should fix another race condition

--- main.go
+++ main.go
@@ -614,19 +614,27 @@ func containsEphemeralConfiguration() bool {
 }
 
 func (run *RunRunner) Run() int {
+	// This is used to wait for possible multiple jobs, they would execute sequentially and we need to wait for all
+	var jobCompletedWG sync.WaitGroup
+	allJobsDone := func() chan struct{} {
+		ch := make(chan struct{})
+		go func() {
+			jobCompletedWG.Wait()
+			ch <- jobCompletedWG
+		}()
+		return ch
+	}
 	container.SetContainerAllocateTerminal(run.Terminal)
 	// trap Ctrl+C
 	channel := make(chan os.Signal, 1)
 	signal.Notify(channel, syscall.SIGTERM, os.Interrupt)
 	ctx, cancel := context.WithCancel(context.Background())
 	firstJobReceived := false
-	jobctx, cancelJob := context.WithCancel(context.Background())
-	cancelJob()
 	go func() {
 		sig := <-channel
 		if sig == syscall.SIGTERM {
 			select {
-			case <-jobctx.Done():
+			case <-allJobsDone():
 				fmt.Println("SIGTERM received, no job is running shutdown")
 			default:
 				fmt.Println("SIGTERM received, cancel the current job and wait for completion")
@@ -634,7 +642,7 @@ func (run *RunRunner) Run() int {
 			cancel()
 		} else {
 			select {
-			case <-jobctx.Done():
+			case <-allJobsDone():
 				fmt.Println("CTRL+C received, no job is running shutdown")
 				cancel()
 			default:
@@ -657,7 +665,7 @@ func (run *RunRunner) Run() int {
 		signal.Stop(channel)
 	}()
 	defer func() {
-		<-jobctx.Done()
+		<-allJobsDone()
 	}()
 	settings, err := loadConfiguration()
 	if err != nil {
@@ -932,10 +940,13 @@ func (run *RunRunner) Run() int {
 								if run.Once {
 									firstJobReceived = true
 								}
-								var finishJob context.CancelFunc
-								jobctx, finishJob = context.WithCancel(context.Background())
-								var jobExecCtx context.Context
-								jobExecCtx, cancelJob = context.WithCancel(ctx)
+								jobctx, finishJob := context.WithCancel(context.Background())
+								jobExecCtx, cancelJob := context.WithCancel(ctx)
+								jobCompletedWG.Add(1)
+								go func() {
+									<-jobctx.Done()
+									jobCompletedWG.Done()
+								}()
 								runJob(vssConnection, run, cancel, cancelJob, finishJob, jobExecCtx, jobctx, session, *message, instance)
 								{
 									message, err = session.GetNextMessage(jobExecCtx)
@@ -974,7 +985,7 @@ func (run *RunRunner) Run() int {
 			return 1
 		}
 		select {
-		case <-jobctx.Done():
+		case <-allJobsDone():
 			if run.Once {
 				return 0
 			}
@@ -1753,7 +1764,7 @@ func runJob(vssConnection *protocol.VssConnection, run *RunRunner, cancel contex
 			logger.Log(logrus.InfoLevel, "Runner Version: "+version)
 
 			// Wait for possible concurrent running job and serialize, this only happens for multi repository runners
-			waitContext, finishWait := context.WithCancel(context.Background())
+			waitContext, finishWait := context.WithCancel(jobExecCtx)
 			go func() {
 				for {
 					select {
