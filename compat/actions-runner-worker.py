3384819fe586e3631f1c4b28fd1ba37d302840fc
ChristopherHX <christopher.homberger@web.de>
2023-08-31 18:22:29 +0200

    Create actions-runner-worker.py

---
+++ compat/actions-runner-worker.py
@@ -0,0 +1,39 @@
+# This script can be used to call Runner.Worker as github-act-runner worker on unix like systems
+# You just have to create simple .runner file in the root folder with the following Content
+# {"workFolder": "_work"}
+# Then use `python3 path/to/this/script.py path/to/actions/runner/bin/Runner.Worker` as the worker args
+
+import sys
+import subprocess
+import os
+import threading
+import codecs
+
+wdr, wdw = os.pipe()
+rdr, rdw = os.pipe()
+
+def redirectio():
+    while(True):
+        stdin = sys.stdin.fileno()
+        messageType = int.from_bytes(os.read(stdin, 4), "big", signed=False)
+        os.write(rdw, messageType.to_bytes(4, sys.byteorder, signed=False))
+        messageLength = int.from_bytes(os.read(stdin, 4), "big", signed=False)
+        message = os.read(stdin, messageLength)
+        encoded = codecs.decode(message, "utf-8").encode("utf_16")[2:]        
+        print(encoded)
+        os.write(rdw, len(encoded).to_bytes(4, sys.byteorder, signed=False))
+        os.write(rdw, encoded)
+
+threading.Thread(target=redirectio, daemon=True).start()
+
+interpreter = []
+worker = sys.argv[1]
+if worker.endswith(".dll"):
+    interpreter = [ "dotnet" ]
+
+code = subprocess.call(interpreter + [worker, "spawnclient", format(rdr), format(wdw)], pass_fds=(rdr, wdw))
+print(code)
+if code >= 100 and code < 105:
+    sys.exit(0)
+else:
+    sys.exit(1)
