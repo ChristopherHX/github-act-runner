c47a88f0ecd77aa75278cc3ce8049faaad9a1323
Christopher Homberger <christopher.homberger@web.de>
2026-03-21 16:30:23 +0100

    .

--- protocol/logger/job_logger.go
+++ protocol/logger/job_logger.go
@@ -194,7 +194,8 @@ func (logger *WebsocketLiveloggerWithFallback) Close() error {
 }
 
 func (logger *WebsocketLiveloggerWithFallback) sendLogFallback(
-	err error, reason string, wrapper *protocol.TimelineRecordFeedLinesWrapper) error {
+	err error, reason string, wrapper *protocol.TimelineRecordFeedLinesWrapper,
+) error {
 	if !logger.ForceWebsock {
 		if logger.Connection.Trace {
 			fmt.Printf("Failed to %s to websocket %s, fallback to vsslogger\n", reason, err.Error())
